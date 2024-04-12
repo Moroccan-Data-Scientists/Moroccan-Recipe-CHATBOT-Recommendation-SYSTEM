587acf1156f5b904f47d565d63061856580f795b
Mohamed-Bencheikh <bencheikhmohamed811@gmail.com>
2024-04-12 20:35:33 +0000

    simple response endpoint

--- web/backend/api.py
+++ web/backend/api.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
+
 
 @app.get("/api/response/")
 def get_response(message):
     return {"message": f"Recieved message: {message}. response: Hello world!"}
+
+
+@app.post('/')
+def response(message: str = Form(...)):
+    # `getResponse` function to be defined later.
+    response = getResponse(message)
+    return response
